0638e267318a2f08f7fddc05fb28791df7b3864f
realredz999 <sasukevwv@gmail.com>
2026-06-18 06:34:38 +0530

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 local PlaceId = game.PlaceId
 
--- List of supported Blox Fruits Place IDs (World 1, World 2, and World 3)
+
 local BloxFruitsIDs = {
     [2753915549] = true,
     [85211729168715] = true,
@@ -11,7 +11,7 @@ local BloxFruitsIDs = {
 }
 
 if BloxFruitsIDs[PlaceId] then
-    -- Load the BloxFruits script from the new repository
+
     loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz999/NewRedz/main/obfed/BloxFruits.lua"))()
 else
     warn("Unsupported Game PlaceId: " .. tostring(PlaceId))
